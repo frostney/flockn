55e44a47ee05ce0fc89e40b4e2f591deaf15c00a
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-29 19:11:04 +0200

    Fixes addable

--- src/addable.js
+++ src/addable.js
@@ -1,11 +1,11 @@
 udefine('addable', function() {
   return function(Factory, groupInstance) {
 
-    return function(child) {
+    return function() {
       var child = arguments[0];
       var args = 2 <= arguments.length ? [].slice.call(arguments, 1) : [];
 
-      if (!( child instanceof child)) {
+      if (!( child instanceof Factory)) {
         if ( typeof child === 'string') {
           if (Object.hasOwnProperty.call(store, child)) {
             child = new Factory(Factory.store[child], args);
