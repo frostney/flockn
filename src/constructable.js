6faca0fbff5bd51a577c4f0dd0b7ff362f5b3409
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-06 13:58:55 +0200

    The old splice vs slice mistake

--- src/constructable.js
+++ src/constructable.js
@@ -1,6 +1,6 @@
 udefine(function() {
 	return function(constructor, type, args) {
-		var argArray = [].splice.call(arguments);
+		var argArray = [].slice.call(arguments);
 		
 		argArray.unshift(type);
 		var Factory = constructor.bind.apply(constructor, argArray);
