a8c45ce0a8e0e66515ef330ee5d39b322462c15d
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-06 14:58:00 +0200

    Only convert to array if it's needed

--- src/constructable.js
+++ src/constructable.js
@@ -1,6 +1,6 @@
 udefine(function() {
 	return function(constructor, type, args) {
-		var argArray = [].slice.call(arguments);
+		var argArray = (Array.isArray(args)) ? args : [].slice.call(args);
 		
 		argArray.unshift(type);
 		var Factory = constructor.bind.apply(constructor, argArray);
