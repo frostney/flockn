b1b69e94c2cd3bd442ed383db281ac8ff7c2d5e9
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-13 15:31:54 +0200

    DOM stub

--- src/graphics/dom.js
+++ src/graphics/dom.js
@@ -3,7 +3,37 @@ udefine(['../graphics'], function(Graphics) {
 	Graphics.init = function(container) {
 		Graphics.renderer = 'DOM';
 		Graphics.container = container;
+		Graphics.element = document.getElementById(container);
+		
+		if (Graphics.element == null) {
+			var element = document.createElement('div');
+			element.id = container;
+			document.body.appendChild(element);
+			
+			Graphics.element = element;
+		}
 	};
+	
+	Graphics.on('create', function(type, obj) {
+		switch (type) {
+			case 'Scene':
+				var element = document.createElement('div');
+				element.id = obj.name;
+				Graphics.element.appendChild(element);
+				break;
+			case 'GameObject':
+				var element = document.createElement('div');
+				var parentElement = document.getElementById(obj.parent.name);
+				element.id = obj.name;
+				element.style.position = 'absolute';
+				element.style.left = obj.x + 'px';
+				element.style.top = obj.y + 'px';
+				parentElement.appendChild(element);
+				break;
+			default:
+				break;
+		}
+	});
 	
 	Graphics.on('render', function(type, obj) {
 		
