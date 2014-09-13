42ca53ab8059c1361910586b70306b2dfda60488
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-13 11:59:54 +0200

    Quick & dirty fix for descriptor calling in Base

--- src/constructable.js
+++ src/constructable.js
@@ -2,6 +2,10 @@ udefine(function() {
 	return function(constructor, type, args) {
 		var argArray = (Array.isArray(args)) ? args : [].slice.call(args);
 		
+		// TODO: Somehow the bind.apply combination kills an argument, so we add one in
+		// before it's property fixed
+		argArray.unshift(constructor);
+		
 		argArray.unshift(type);
 		var Factory = constructor.bind.apply(constructor, argArray);
 		return new Factory();
