7228f99fbb43136e1753b076d08ed394c0098375
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-13 13:13:41 +0200

    Base has an extend function

--- src/base.js
+++ src/base.js
@@ -1,26 +1,42 @@
 udefine(['eventmap', 'mixedice', './group'], function(EventMap, mixedice, Group) {
-  
-  var Base = function() {
+  'use strict';
+
+  var Base = function(type) {
     mixedice([this, Base.prototype], new EventMap());
-    
-    var type = arguments[0];
-    var descriptor = arguments[1];
-    var args = 3 <= arguments.length ? [].slice.call(arguments, 2) : [];
-    
+
+    type = type || 'Base';
+
     this.type = type;
     this.name = this.type + '-' + Date.now();
-    
+
     this.children = new Group();
-    
+
     this.parent = null;
-    
-    if (descriptor) {
-    	descriptor.apply(this, args);
-    }
-    
-    this.trigger('create');
+
+    this.trigger('constructed');
   };
-  
+
+  Base.extend = function(target, type) {
+    var base = new Base(type);
+
+    mixedice(target, base);
+
+    return {
+      factory: function(args) {
+        var descriptor;
+
+        if (args) {
+          descriptor = args[0];
+          var argsArray = 2 <= args.length ? [].slice.call(args, 1) : [];
+        }
+
+        if (descriptor) {
+          descriptor.apply(this || base, argsArray);
+        }
+      }
+    };
+  };
+
   return Base;
-  
+
 });
