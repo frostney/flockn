5675d528e5e369f2bbce438a4932c51c1c7fdc21
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-13 15:30:53 +0200

    Descriptor is now publicly available

--- src/base.js
+++ src/base.js
@@ -1,56 +1,40 @@
-udefine(['eventmap', 'mixedice', './group'], function(EventMap, mixedice, Group) {
+udefine(['eventmap', 'mixedice', './graphics', './group'], function(EventMap, mixedice, Graphics, Group) {
   'use strict';
 
-  var Base = function(type) {
+  var Base = function(type, descriptor) {
     var self = this;
-    
+
     mixedice([this, Base.prototype], new EventMap());
 
     type = type || 'Base';
 
     this.type = type;
     this.name = this.type + '-' + Date.now();
+    this.descriptor = descriptor;
 
     this.children = new Group();
 
     this.parent = null;
-    
-    this.on('render', function() {
-    	Graphics.trigger('render', self.type, self);
-    	
-      self.children.forEach(function(child) {
-        child.trigger('render');
-      });
-    });
-    
-    this.on('update', function() {
-      self.children.forEach(function(child) {
-        child.trigger('update');
-      });
-    });
 
     this.trigger('constructed');
   };
 
-  Base.extend = function(target, type) {
-    var base = new Base(type);
+  Base.prototype.call = function() {
+    this.apply(arguments);
+  };
+
+  Base.prototype.apply = function(args) {
+    if (this.descriptor) {
+      this.descriptor.apply(this, args);
+      
+      this.trigger('execute');
+    }
+  };
+
+  Base.extend = function(target, type, descriptor) {
+    var base = new Base(type, descriptor);
 
     mixedice(target, base);
-
-    return {
-      factory: function(args) {
-        var descriptor;
-
-        if (args) {
-          descriptor = args[0];
-          var argsArray = 2 <= args.length ? [].slice.call(args, 1) : [];
-        }
-
-        if (descriptor) {
-          descriptor.apply(this || base, argsArray);
-        }
-      }
-    };
   };
 
   return Base;
