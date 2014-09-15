e2bdc28fd1b2639863a1ad02fabdceaadd1116dd
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-15 14:02:28 +0200

    More work on the  DOM renderer

--- src/graphics/dom.js
+++ src/graphics/dom.js
@@ -1,56 +1,84 @@
 udefine(['../graphics'], function(Graphics) {
-	
-	Graphics.init = function(container) {
-		Graphics.renderer = 'DOM';
-		
-		if (container.indexOf('#') === 0) {
-			container = container.slSice(1);
-		}
-		
-		Graphics.container = container;
-		Graphics.element = document.getElementById(container);
-		
-		if (Graphics.element == null) {
-			var element = document.createElement('div');
-			element.id = container;
-			document.body.appendChild(element);
-			
-			Graphics.element = element;
-		}
-	};
-	
-	Graphics.on('create', function(type, obj) {
-		switch (type) {
-			case 'Scene':
-				var element = document.createElement('div');
-				element.id = obj.name;
-				Graphics.element.appendChild(element);
-				break;
-			case 'GameObject':
-				var element = document.createElement('div');
-				var parentElement = document.getElementById(obj.parent.name);
-				element.id = obj.name;
-				element.style.position = 'absolute';
-				element.style.left = obj.x + 'px';
-				element.style.top = obj.y + 'px';
-				parentElement.appendChild(element);
-				break;
-			default:
-				break;
-		}
-	});
-	
-	Graphics.on('render', function(type, obj) {
-		
-		switch (type) {
-			case 'GameObject':
-				break;
-			default:
-				break;
-		}
-		
-	});
-	
-	return Graphics;
-	
+
+  Graphics.init = function(container, width, height) {
+    Graphics.renderer = 'DOM';
+
+    if (container.indexOf('#') === 0) {
+      container = container.slice(1);
+    }
+
+    width = width || 600;
+    height = height || 480;
+
+    Graphics.container = container;
+    Graphics.element = document.getElementById(container);
+
+    if (Graphics.element == null) {
+      var element = document.createElement('div');
+      element.id = container;
+      document.body.appendChild(element);
+
+      Graphics.element = element;
+    }
+  };
+
+  Graphics.on('add', function(obj) {
+    // Remove previous elements of the same id
+    if (document.getElementById(obj.name) != null) {
+
+    }
+
+    var parent = obj.parent;
+
+    var parentElem = (function() {
+      if ((parent && parent.isRoot) || parent == null) {
+        return Graphics.element;
+      } else {
+        var element = document.getElementById(obj.parent);
+        if (element == null) {
+          return Graphics.element;
+        } else {
+          return element;
+        }
+      }
+    })();
+
+    /*if (parent == null) {
+      console.warn('Parent (' + obj.parent.name + ') for object (' + obj.name + ') does not exist. Aborting render call for this object.');
+      return;
+    }*/
+
+    var element = document.createElement('div');
+    element.id = obj.id.toLowerCase();
+    element.className = [obj.type.toLowerCase(), obj.name.toLowerCase()].join(' ');
+    element.style.position = 'absolute';
+
+    switch (obj.type) {
+    case 'Scene':
+      break;
+    case 'GameObject':
+      element.style.left = obj.x + 'px';
+      element.style.top = obj.y + 'px';
+      break;
+    default:
+      break;
+    }
+
+    parentElem.appendChild(element);
+  });
+
+  Graphics.on('render', function(obj) {
+    // Update element attributes
+
+    switch (obj.type) {
+    case 'GameObject':
+      break;
+    default:
+      break;
+    }
+
+  });
+
+  return Graphics;
+
 });
