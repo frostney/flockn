a82c831b017c5e4f10127424629c30ca0fd1c806
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-16 11:39:05 +0200

    Fixes rendering for GameObjects

--- src/graphics/dom.js
+++ src/graphics/dom.js
@@ -1,5 +1,13 @@
 udefine(['../graphics'], function(Graphics) {
 
+  var pixelize = function(num) {
+    return num + 'px';
+  };
+
+  var unpixelize = function(str) {
+    return parseFloat(str) || 0;
+  };
+
   Graphics.init = function(container, width, height) {
     Graphics.renderer = 'DOM';
 
@@ -20,6 +28,10 @@ udefine(['../graphics'], function(Graphics) {
 
       Graphics.element = element;
     }
+
+    Graphics.element.style.width = pixelize(width);
+    Graphics.element.style.height = pixelize(height);
+
   };
 
   Graphics.on('add', function(obj) {
@@ -34,7 +46,7 @@ udefine(['../graphics'], function(Graphics) {
       if ((parent && parent.isRoot) || parent == null) {
         return Graphics.element;
       } else {
-      	var parentId = obj.parent.id.toLowerCase();
+        var parentId = obj.parent.id.toLowerCase();
         var element = document.getElementById(parentId);
         if (element == null) {
           return Graphics.element;
@@ -43,11 +55,6 @@ udefine(['../graphics'], function(Graphics) {
         }
       }
     })();
-
-    /*if (parent == null) {
-      console.warn('Parent (' + obj.parent.name + ') for object (' + obj.name + ') does not exist. Aborting render call for this object.');
-      return;
-    }*/
 
     var element = document.createElement('div');
     element.id = obj.id.toLowerCase();
@@ -58,8 +65,10 @@ udefine(['../graphics'], function(Graphics) {
     case 'Scene':
       break;
     case 'GameObject':
-      element.style.left = obj.x + 'px';
-      element.style.top = obj.y + 'px';
+      element.style.left = pixelize(obj.x);
+      element.style.top = pixelize(obj.y);
+      element.style.width = pixelize(obj.width);
+      element.style.height = pixelize(obj.height);
       break;
     default:
       break;
@@ -68,14 +77,55 @@ udefine(['../graphics'], function(Graphics) {
     parentElem.appendChild(element);
   });
 
+  Graphics.on('texture-loaded', function(obj, texture) {
+    var element = document.getElementById(obj.id.toLowerCase());
+
+    if (element != null) {
+      element.style.backgroundImage = 'url(' + texture.filename + ')';
+      element.style.width = pixelize(obj.width);
+      element.style.height = pixelize(obj.height);
+    }
+  });
+
   Graphics.on('render', function(obj) {
     // Update element attributes
+    var element = document.getElementById(obj.id.toLowerCase());
 
-    switch (obj.type) {
-    case 'GameObject':
-      break;
-    default:
-      break;
+    if (element != null) {
+      switch (obj.type) {
+      case 'GameObject':
+        var elemX = unpixelize(element.style.left);
+        var elemY = unpixelize(element.style.top);
+        var elemWidth = unpixelize(element.style.width);
+        var elemHeight = unpixelize(element.style.height);
+
+        if (elemX !== obj.x) {
+          element.style.left = pixelize(obj.x);
+        }
+
+        if (elemY !== obj.y) {
+          element.style.top = pixelize(obj.y);
+        }
+
+        if (elemWidth !== obj.width) {
+          element.style.width = pixelize(obj.width);
+        }
+
+        if (elemHeight !== obj.height) {
+          element.style.height = pixelize(obj.height);
+        }
+        
+        // Set background color
+        if (!obj.texture.filename) {
+        	element.style.backgroundColor = obj.texture.color;
+        }
+        break;
+      case 'Game':
+      	break;
+      default:
+      	break;
+      }
+
     }
 
   });
