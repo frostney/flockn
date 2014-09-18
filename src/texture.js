6f40eb1b260ca4784fb2e158db62040a56a9d5a9
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-18 13:46:55 +0200

    Texture now has .image and .label property

--- src/texture.js
+++ src/texture.js
@@ -1,68 +1,77 @@
 udefine(['mixedice', 'eventmap'], function(mixedice, EventMap) {
-	'use strict';
-	
-	var Texture = function() {
-		mixedice([this, Texture.prototype], new EventMap());
-		
-		var self = this;
-		
-		var filename = '';
-		
-		this.width = 0;
-		this.height = 0;
-		
-		this.data = null;
-		
-		this.parent = null;
-		
-		this.offset = {
-			x: 0,
-			y: 0
-		};
-		
-		// TODO: Filename is not clear enough, maybe
-		// image.filename
-		Object.defineProperty(this, 'filename', {
-			get: function() {
-				return filename;
-			},
-			set: function(value) {
-				filename = value;
-				
-				// TODO: Most of this should already be handled by the preloader
-				var img = new Image();
-				img.src = filename;
-				
-				img.onload = function() {
-					self.data = img;
-					self.trigger('loaded');
-				};
-			},
-			enumerable: true
-		});
-		
-		var label = {
-			text: '',
-			font: {
-				size: 10,
-				name: 'Arial',
-				color: 'rgb(0, 0, 0)'
-			}
-		};
-		
-		Object.defineProperty(this, 'label', {
-			get: function() {
-				return {};
-			},
-			set: function(value) {
-				
-			},
-			enumerable: true
-		});
-		
-		this.color = 'rgb(255, 255, 255)';
-		
-	};
-	
-	return Texture;
+  'use strict';
+
+  var Texture = function() {
+    mixedice([this, Texture.prototype], new EventMap());
+
+    var self = this;
+
+    this.width = 0;
+    this.height = 0;
+
+    this.parent = null;
+
+    this.image = {
+      offset: {
+        x: 0,
+        y: 0
+      },
+      data: null,
+      width: 0,
+      height: 0
+    };
+
+    var filename = '';
+
+    // TODO: Filename is not clear enough, maybe
+    // image.filename
+    Object.defineProperty(this.image, 'filename', {
+      get: function() {
+        return filename;
+      },
+      set: function(value) {
+        filename = value;
+
+        // TODO: Most of this should already be handled by the preloader
+        var img = new Image();
+        img.src = filename;
+
+        img.onload = function() {
+          self.image.data = img;
+          self.image.width = img.width;
+          self.image.height = img.height;
+          
+          self.trigger('loaded-image');
+        };
+      },
+      enumerable: true
+    });
+
+
+    this.label = {
+      font: {
+        size: 10,
+        name: 'Arial',
+        color: 'rgb(0, 0, 0)'
+      }
+    };
+    
+    var text = '';
+    
+    Object.defineProperty(this.label, 'text', {
+    	get: function() {
+    		return text;
+    	},
+    	set: function(value) {
+    		text = value;
+    		
+    		self.trigger('loaded-label');
+    	}
+    });
+
+    this.color = 'rgb(255, 255, 255)';
+
+  };
+
+  return Texture;
 });
