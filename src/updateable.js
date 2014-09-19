aca53816fe1a240cd2c97a60317d3bd8455a5ba7
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-19 17:49:54 +0200

    Update the updateable documentation

--- src/updateable.js
+++ src/updateable.js
@@ -4,6 +4,7 @@ udefine(function() {
   return function() {
     var self = this;
 
+    // Update all children
     this.on('update', function(dt) {
       self.children.forEach(function(child) {
         child.trigger('update', dt);
