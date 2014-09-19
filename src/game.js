dc0dfb7609b3bfad656b25141bb296ad1b70dda7
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-19 16:17:51 +0200

    Update Game documentation

--- src/game.js
+++ src/game.js
@@ -2,51 +2,59 @@ udefine(['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphic
   'use strict';
 
   var Game = function(descriptor) {
+    // The new operator does not need to be set explicitly.
+    // If it isn't we return an instance of `Game`
     if (!this || !this instanceof Game) {
       return new Game(descriptor);
     }
 
     var self = this;
 
+    // Extend the `Base` class
     Base.extend([this, Game.prototype], 'Game', function() {
       descriptor.call(this);
 
       Graphics.trigger('initialize', this);
     });
 
+    // `this.container` is a string, which is the id of the element. 
+    // If it's not given, it should create a new element. This should be handled by the renderer.
     this.container = null;
+    
+    // By default, the width and height of a `Game` instance will be as large as the inside of the browser window.
     this.width = root.innerWidth;
     this.height = root.innerHeight;
     this.color = 'rgb(255, 255, 255)';
     
+    // `this.activeScene` is set to `null` by default, but will change once a scene will be shown
     this.activeScene = null;
 
-    // Game is the root element so the descriptor needs to be called directly
+    // A `Game` instance is the root element so the descriptor needs to be called directly, 
     // because it won't be added to anywhere else
     this.call();
 
-    // Mix in renderable and updateable
+    // Mix in `renderable` and `updateable`
     renderable.call(this);
     updateable.call(this);
 
-    // Bind the game loop to the update event
+    // Bind the game loop to the `update` event
     Loop.on('update', function(dt) {
-      // Deltatime should not be a millisecond value, but a second one
+      // Deltatime should not be a millisecond value, but a second one. 
       // It should be a value between 0 - 1
       self.trigger('update', dt / 1000);
     });
 
-    // Bind the game loop to the render event
+    // Bind the game loop to the `render` event
     Loop.on('render', function() {
       self.trigger('render');
     });
 
-    // Add a resize event to the Game class
+    // Add a `resize` event to each `Game` instance
     root.addEventListener('resize', function() {
       self.trigger('resize');
     }, false);
 
-    // Add an orientationchange event to the Game class
+    // Add an `orientationchange` event to each `Game` instance
     root.addEventListener('orientationchange', function() {
       self.trigger('orientationchange');
     }, false);
@@ -54,7 +62,7 @@ udefine(['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphic
 
   Game.prototype.addScene = function() {
     // When adding a scene, the dimension of scenes should be
-    // exactly as large as the Game itself
+    // exactly as large as the `Game` instance itself
     this.queue.push(addable(Scene, this.children, function(child) {
       child.width = this.width;
       child.height = this.height;
@@ -64,10 +72,10 @@ udefine(['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphic
   Game.prototype.showScene = function(name) {
     // TODO: Add transitions
     
-    // Set the activeScene property
+    // Set the `activeScene` property
     this.activeScene = name;
     
-    // Trigger the show event
+    // Trigger the `show` event
     this.trigger('show', this.activeScene, this.children[this.activeScene]);
   };
 
