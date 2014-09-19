f9cdba4ec807a82c4194a4b12015954a132fbce6
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-19 17:52:16 +0200

    Add graphics documentation

--- src/graphics/index.js
+++ src/graphics/index.js
@@ -1,7 +1,14 @@
 udefine(['eventmap'], function(EventMap) {
   'use strict';
 
+  // `Graphics` is an instance of an `EventMap`
   var Graphics = new EventMap();
+
+  // Special property `renderer` can be modified, but not deleted
+  Object.defineProperty(Graphics, 'renderer', {
+    value: null,
+    enumerable: true
+  });
 
   return Graphics;
 });
