1c52505f45905d2b51e1a4a635f3d26a4e1c5d8a
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-19 17:50:06 +0200

    Update the renderable documentation

--- src/renderable.js
+++ src/renderable.js
@@ -5,8 +5,10 @@ udefine(['./graphics'], function(Graphics) {
     var self = this;
 
     this.on('render', function() {
+      // Emit `render` event on the `Graphics` object
       Graphics.trigger('render', self);
 
+      // Render all children elements
       self.children.forEach(function(child) {
         child.trigger('render');
       });
