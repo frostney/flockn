bcb0e4c4a6b1ea4a3107aa620a8330e4fee4c21c
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-19 16:02:07 +0200

    Update Game class documentation

--- src/game.js
+++ src/game.js
@@ -18,30 +18,43 @@ udefine(['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphic
     this.width = root.innerWidth;
     this.height = root.innerHeight;
     this.color = 'rgb(255, 255, 255)';
+    
+    this.activeScene = null;
 
+    // Game is the root element so the descriptor needs to be called directly
+    // because it won't be added to anywhere else
     this.call();
 
+    // Mix in renderable and updateable
     renderable.call(this);
     updateable.call(this);
 
+    // Bind the game loop to the update event
     Loop.on('update', function(dt) {
+      // Deltatime should not be a millisecond value, but a second one
+      // It should be a value between 0 - 1
       self.trigger('update', dt / 1000);
     });
 
+    // Bind the game loop to the render event
     Loop.on('render', function() {
       self.trigger('render');
     });
 
+    // Add a resize event to the Game class
     root.addEventListener('resize', function() {
       self.trigger('resize');
     }, false);
 
+    // Add an orientationchange event to the Game class
     root.addEventListener('orientationchange', function() {
       self.trigger('orientationchange');
     }, false);
   };
 
   Game.prototype.addScene = function() {
+    // When adding a scene, the dimension of scenes should be
+    // exactly as large as the Game itself
     this.queue.push(addable(Scene, this.children, function(child) {
       child.width = this.width;
       child.height = this.height;
@@ -50,11 +63,16 @@ udefine(['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphic
 
   Game.prototype.showScene = function(name) {
     // TODO: Add transitions
+    
+    // Set the activeScene property
     this.activeScene = name;
+    
+    // Trigger the show event
     this.trigger('show', this.activeScene, this.children[this.activeScene]);
   };
 
   Game.prototype.run = function(name) {
+    // Start the game loop
     Loop.run();
 
     if (!name) {
@@ -64,6 +82,7 @@ udefine(['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphic
       }
     }
 
+    // Show the scene if a parameter has been specified
     if (name) {
       this.showScene(name);
     }
