4adeb3cbc40e9af2e07d0047796c5049a0e0b995
Johannes Stein <johannesstein@freeze-dev.com>
2014-09-19 18:46:52 +0200

    Update distribution files

--- dist/files/flockn.js
+++ dist/files/flockn.js
@@ -73,6 +73,7 @@ udefine('flockn/base', ['eventmap', 'mixedice', 'gameboard/input', './group', '.
   var Base = function(type, descriptor) {
     var self = this;
 
+    // Mix in an `EventMap` instance into `Base`
     mixedice([this, Base.prototype], new EventMap());
 
     type = type || 'Base';
@@ -80,8 +81,10 @@ udefine('flockn/base', ['eventmap', 'mixedice', 'gameboard/input', './group', '.
     this.type = type;
     this.name = this.type + '-' + Date.now();
 
+    // Count up `objectIndex` and stringify it
     var currentObject = numToIdString(++objectIndex);
 
+    // The `id` property is read-only and returns the type and the stringified object index
     Object.defineProperty(this, 'id', {
       get: function() {
         return this.type + '-' + currentObject;
@@ -89,37 +92,54 @@ udefine('flockn/base', ['eventmap', 'mixedice', 'gameboard/input', './group', '.
       enumerable: true
     });
 
+    // Save the descriptor
     this.descriptor = descriptor;
 
+    // Create a new group for all children elements
     this.children = new Group();
 
+    // Add a queue: All addable elements will be pushed into the queue first and called after everything else in
+    // the `descriptor` has been called
     this.queue = [];
 
     this.parent = null;
 
+    // `Input` should be available in instances derived from `Base`
     this.input = Input;
 
+    // As should `World`
     this.world = World;
 
+    // Emit an event
     this.trigger('constructed');
   };
 
   Base.prototype.call = Base.prototype.reset = function() {
+    // Call `Base#apply` with the arguments object
     this.apply(arguments);
   };
 
   Base.prototype.apply = function(args) {
     // TODO: Reflect if function check should be enforced here
     if (this.descriptor) {
+      // If args is not an array or array-like, provide an empty one
+      args = args || [];
 
+      // Call the `descriptor` property with `args`
       this.descriptor.apply(this, args);
+      
+      // Trigger an event
       this.trigger('execute');
 
       // TODO: Impose an order in the queue, such as:
       // (Game) -> Scene -> GameObject -> Behavior -> Model
+      
+      // TODO: Implement z-order
       this.queue.forEach(function(q) {
         q && q();
       });
+      
+      // Reset the queue
       this.queue = [];
     }
   };
@@ -128,6 +148,7 @@ udefine('flockn/base', ['eventmap', 'mixedice', 'gameboard/input', './group', '.
     if (console && console.log) {
       var argArray = [].slice.call(arguments);
 
+      // Log with `console.log`: Prepend the type and name
       argArray.unshift(':');
       argArray.unshift(this.name);
       argArray.unshift(this.type);
@@ -136,6 +157,7 @@ udefine('flockn/base', ['eventmap', 'mixedice', 'gameboard/input', './group', '.
     }
   };
 
+  // Shorthand function to derive from the Base object
   Base.extend = function(target, type, descriptor) {
     var base = new Base(type, descriptor);
 
@@ -148,20 +170,26 @@ udefine('flockn/base', ['eventmap', 'mixedice', 'gameboard/input', './group', '.
 udefine('flockn/behavior', ['mixedice', './addable', './base', './group', './updateable'], function(mixedice, addable, Base, Group, updateable) {
   'use strict';
 
+  // Behaviors only provide logic. There is no rendering involved.
+  // Behaviors can attach any number of behaviors to itself
   var Behavior = function(descriptor) {
     Base.extend([this, Behavior.prototype], 'Behavior', descriptor);
 
+    // Reference to the game object itself
     this.gameObject = null;
 
+    // Mix in `updateable`
     updateable.call(this);
   };
 
   Behavior.prototype.addBehavior = function() {
+    // When a behavior is added, the reference to the game object is set
     this.queue.push(addable(Behavior, this.children, function(child) {
       child.gameObject = this.gameObject;
     }).apply(this, arguments));
   };
 
+  // Behaviors can be defined and are stored on the object itself
   Behavior.store = {};
 
   Behavior.define = function(name, factory) {
@@ -174,47 +202,70 @@ udefine('flockn/behavior', ['mixedice', './addable', './base', './group', './upd
 udefine('flockn/game', ['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphics', './scene', './renderable', './updateable'], function(root, mixedice, Loop, addable, Base, Graphics, Scene, renderable, updateable) {
   'use strict';
 
+  // Game is the entry point for all games made with flockn.
+  // Any number of `Scene` instances can be attached to a `Game` instance
   var Game = function(descriptor) {
+    // The new operator does not need to be set explicitly.
+    // If it isn't we return an instance of `Game`
     if (!this || !this instanceof Game) {
       return new Game(descriptor);
     }
 
     var self = this;
 
+    // Extend the `Base` class
     Base.extend([this, Game.prototype], 'Game', function() {
       descriptor.call(this);
 
       Graphics.trigger('initialize', this);
     });
 
+    // `this.container` is a string, which is the id of the element. 
+    // If it's not given, it should create a new element. This should be handled by the renderer.
     this.container = null;
+    
+    // By default, the width and height of a `Game` instance will be as large as the inside of the browser window.
     this.width = root.innerWidth;
     this.height = root.innerHeight;
     this.color = 'rgb(255, 255, 255)';
+    
+    // `this.activeScene` is set to `null` by default, but will change once a scene will be shown
+    this.activeScene = null;
 
+    // A `Game` instance is the root element so the descriptor needs to be called directly, 
+    // because it won't be added to anywhere else
     this.call();
 
+    // Mix in `renderable` and `updateable`
     renderable.call(this);
     updateable.call(this);
 
+    // Bind the game loop to the `update` event
     Loop.on('update', function(dt) {
+      // Deltatime should not be a millisecond value, but a second one. 
+      // It should be a value between 0 - 1
       self.trigger('update', dt / 1000);
     });
 
+    // Bind the game loop to the `render` event
     Loop.on('render', function() {
       self.trigger('render');
     });
 
+    // Add a `resize` event to each `Game` instance
     root.addEventListener('resize', function() {
       self.trigger('resize');
     }, false);
 
+    // Add an `orientationchange` event to each `Game` instance
     root.addEventListener('orientationchange', function() {
       self.trigger('orientationchange');
     }, false);
   };
 
   Game.prototype.addScene = function() {
+    // When adding a scene, the dimension of scenes should be
+    // exactly as large as the `Game` instance itself
     this.queue.push(addable(Scene, this.children, function(child) {
       child.width = this.width;
       child.height = this.height;
@@ -223,11 +274,16 @@ udefine('flockn/game', ['root', 'mixedice', 'gameboard/loop', './addable', './ba
 
   Game.prototype.showScene = function(name) {
     // TODO: Add transitions
+    
+    // Set the `activeScene` property
     this.activeScene = name;
+    
+    // Trigger the `show` event
     this.trigger('show', this.activeScene, this.children[this.activeScene]);
   };
 
   Game.prototype.run = function(name) {
+    // Start the game loop
     Loop.run();
 
     if (!name) {
@@ -237,6 +293,7 @@ udefine('flockn/game', ['root', 'mixedice', 'gameboard/loop', './addable', './ba
       }
     }
 
+    // Show the scene if a parameter has been specified
     if (name) {
       this.showScene(name);
     }
@@ -300,8 +357,11 @@ udefine('flockn/gameobject', ['mixedice', './addable', './base', './behavior', '
 
     this.fitToTexture = true;
 
+    // Create a new texture and bind it to the `texture` property
     this.texture = new Texture();
     this.texture.parent = this;
+    
+    // Once the image is loaded, update width and height if `fitToTexture` is set
     this.texture.on('image-loaded', function() {
       if (self.fitToTexture) {
         self.width = self.texture.image.width;
@@ -315,6 +375,7 @@ udefine('flockn/gameobject', ['mixedice', './addable', './base', './behavior', '
       Graphics.trigger('texture-image-loaded', self, self.texture);
     });
 
+    // Once the label is loaded, update width and height if `fitToTexture` is set
     this.texture.on('label-loaded', function() {
       if (self.fitToTexture) {
         self.width = self.texture.label.width;
@@ -357,9 +418,11 @@ udefine('flockn/gameobject', ['mixedice', './addable', './base', './behavior', '
     // Data models
     this.models = new Group();
 
+    // Mix in renderable and updateable
     renderable.call(this);
     updateable.call(this);
 
+    // Update all behaviors as well
     this.on('update', function() {
       self.behaviors.forEach(function(behavior) {
         behavior.trigger('update');
@@ -369,25 +432,30 @@ udefine('flockn/gameobject', ['mixedice', './addable', './base', './behavior', '
 
   GameObject.store = {};
 
+  // Game objects can be defined and are stored on the object itself
   GameObject.define = function(name, factory) {
     GameObject.store[name] = factory;
   };
 
   GameObject.prototype.addGameObject = function() {
+    // Add a game object to this game object
     this.queue.push(addable(GameObject, this.children).apply(this, arguments));
   };
 
   GameObject.prototype.addBehavior = function() {
+    // Add a `Behavior` instance to the the game object and update the `gameObject` property
     this.queue.push(addable(Behavior, this.behaviors, function(child) {
       child.gameObject = this;
     }).apply(this, arguments));
   };
 
   GameObject.prototype.addModel = function() {
+    // Add a `Model` instance to the game object
     this.queue.push(addable(Model, this.models).apply(this, arguments));
   };
 
   GameObject.prototype.toJSON = function() {
+    // Serialize this object
     return serialize(this);
   };
 
@@ -396,6 +464,7 @@ udefine('flockn/gameobject', ['mixedice', './addable', './base', './behavior', '
   };
 
   GameObject.prototype.animate = function(property, end, time, callback) {
+    // TODO: Tweening does not work yet
     if ( typeof this[property] === 'number') {
       var distance = end - this[property];
       var timeInS = (time / 1000);
@@ -414,7 +483,15 @@ udefine('flockn/gameobject', ['mixedice', './addable', './base', './behavior', '
 udefine('flockn/graphics', ['eventmap'], function(EventMap) {
   'use strict';
 
+  // `Graphics` is an instance of an `EventMap`
   var Graphics = new EventMap();
+
+  // Special property `renderer` can be modified, but not deleted
+  Object.defineProperty(Graphics, 'renderer', {
+    value: null,
+    writable: true,
+    enumerable: true
+  });
 
   return Graphics;
 });
@@ -423,6 +500,8 @@ udefine('flockn/graphics/rootelement', function() {
   'use strict';
 
   return function(elementName, extraFn) {
+    // Sets the container name: If none is given, set the id of the object. 
+    // If a `#` is prepended to the string, cut it off
     var containerName = (function() {
       if (this.container == null) {
         this.container = this.id;
@@ -434,11 +513,14 @@ udefine('flockn/graphics/rootelement', function() {
       }
     }).call(this);
 
+    // Set the dimensions of the object. If none are given, it should be the inside of the browser's window
     this.width = this.width || window.innerWidth;
     this.height = this.height || window.innerHeight;
 
+    // Try to get the HTML element by using `containerName`
     var rootElement = document.getElementById(containerName);
 
+    // If nothing was found, create the element
     if (rootElement == null) {
       var element = document.createElement(elementName);
       element.id = containerName.toLowerCase();
@@ -449,12 +531,17 @@ udefine('flockn/graphics/rootelement', function() {
 
     rootElement.className = [this.type.toLowerCase(), this.name.toLowerCase()].join(' ');
 
+    // Set the dimensions of the `rootElement`
     rootElement.style.position = 'absolute';
     rootElement.style.width = this.width + 'px';
     rootElement.style.height = this.height + 'px';
 
+    // Allow some extra functionality to happen here.
+    // It should be called on the same context and the
+    // `rootElement` is passed in as a parameter
     extraFn.call(this, rootElement);
 
+    // Center the element if it's smaller than the inside of the browser's window
     if (this.width < window.innerWidth) {
       rootElement.style.left = '50%';
       rootElement.style.marginLeft = (this.width * (-0.5)) + 'px';
@@ -465,6 +552,7 @@ udefine('flockn/graphics/rootelement', function() {
       rootElement.style.marginTop = (this.width * (-0.5)) + 'px';
     }
 
+    // Return the element, in case someone wants to meddle with it
     return rootElement;
   };
 });
@@ -577,19 +665,24 @@ udefine('flockn/model', ['mixedice', 'eventmap'], function(mixedice, EventMap) {
   'use strict';
 
   var Model = function() {
+    // Mix in `EventMap` into all `Model` instances
     mixedice([this, Model.prototype], new EventMap());
-
+    
+    // Store attribute data
     this.data = {};
   };
 
   Model.prototype.get = function(name) {
+    // Get an attribute if it exists
     if (Object.hasOwnProperty.call(this.data, name)) {
       return this.data[name];
     }
   };
 
   Model.prototype.set = function(name, value) {
+    // Set or add an attribute
     this.data[name] = value;
+    // Trigger the `change` event with `name` and `value` as its parameters
     this.trigger('change', name, value);
   };
 
@@ -604,8 +697,10 @@ udefine('flockn/renderable', ['./graphics'], function(Graphics) {
     var self = this;
 
     this.on('render', function() {
+      // Emit `render` event on the `Graphics` object
       Graphics.trigger('render', self);
 
+      // Render all children elements
       self.children.forEach(function(child) {
         child.trigger('render');
       });
@@ -616,19 +711,25 @@ udefine('flockn/renderable', ['./graphics'], function(Graphics) {
 udefine('flockn/scene', ['mixedice', './addable', './base', './group', './gameobject', './renderable', './updateable'], function(mixedice, addable, Base, Group, GameObject, renderable, updateable) {
   'use strict';
 
+  // A `Scene` instance is a layer for `GameObject` instances.
+  // Any number of game objects can be added to a scene. Only one scene should be visible at the same time, depending
+  // on what was set in the `activeScene` property of a `Game` instance.
   var Scene = function(descriptor) {
     Base.extend([this, Scene.prototype], 'Scene', descriptor);
 
+    // Mix in `renderable` and `updateable`
     renderable.call(this);
     updateable.call(this);
   };
 
   Scene.prototype.addGameObject = function() {
+    // Allow game objects to be added to scenes
     this.queue.push(addable(GameObject, this.children).apply(this, arguments));
   };
 
   Scene.store = {};
 
+  // Scenes can be defined and are stored on the object itself
   Scene.define = function(name, factory) {
     Scene.store[name] = factory;
   };
@@ -640,6 +741,7 @@ udefine('flockn/scene', ['mixedice', './addable', './base', './group', './gameob
 udefine('flockn/serialize', function() {
   'use strict';
 
+  // Serialize function to `JSON.stringify` with a custom replacer
   return function(obj) {
     return JSON.stringify(obj, function(key, value) {
       // Avoiding cyclic dependencies
@@ -661,15 +763,19 @@ udefine('flockn/texture', ['mixedice', 'eventmap'], function(mixedice, EventMap)
   'use strict';
 
   var Texture = function() {
+    // Mix in an `EventMap` instance into the `Texture`
     mixedice([this, Texture.prototype], new EventMap());
 
     var self = this;
 
+    // Set up dimensions
     this.width = 0;
     this.height = 0;
 
+    // Set parent property
     this.parent = null;
 
+    // The default values for `image`
     this.image = {
       color: 'rgb(255, 255, 255)',
       offset: {
@@ -705,6 +811,7 @@ udefine('flockn/texture', ['mixedice', 'eventmap'], function(mixedice, EventMap)
       enumerable: true
     });
 
+    // Default value for `label`
     this.label = {
       font: {
         size: 10,
@@ -729,6 +836,7 @@ udefine('flockn/texture', ['mixedice', 'eventmap'], function(mixedice, EventMap)
       set: function(value) {
         text = value;
 
+        // Calculate the size of the label and update the dimensions
         // TODO: This should be handled somewhere else, but I'm not sure where
         var tmpElem = document.createElement('div');
         tmpElem.innerText = text;
@@ -779,6 +887,7 @@ udefine('flockn/updateable', function() {
   return function() {
     var self = this;
 
+    // Update all children
     this.on('update', function(dt) {
       self.children.forEach(function(child) {
         child.trigger('update', dt);
@@ -790,6 +899,7 @@ udefine('flockn/updateable', function() {
 udefine('flockn/world', ['./model'], function(Model) {
   'use strict';
 
+  // `World` is an instance of a model
   var world = new Model();
 
   return world;
