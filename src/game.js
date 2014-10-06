2c66a80609e6732518cf2611f2609aea6f1518bb
Johannes Stein <johannesstein@freeze-dev.com>
2014-10-06 23:20:53 +0200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -77,6 +77,9 @@ udefine(['root', 'mixedice', 'gameboard/loop', './addable', './base', './graphic
     // Set the `activeScene` property
     this.activeScene = name;
     
+    // Call resize event
+    this.activeScene.trigger('resize', root.innerWidth, root.innerHeight);
+    
     // Trigger the `show` event
     this.trigger('show', this.activeScene, this.children[this.activeScene]);
   };
