c5858e61a527b84bbd3e753717dbbd27a1aea142
Johannes Stein <johannesstein@freeze-dev.com>
2014-10-21 22:54:09 +0200

    Update vector type

--- src/types/vector2.js
+++ src/types/vector2.js
@@ -1,12 +1,22 @@
 define(function() {
   
+  var sqrMagnitude = function(v) {
+    return Vector2.dot(v, v);
+  };
+  
   var Vector2 = function(x, y) {
     this.x = this.x || 0;
     this.y = this.y || 0;
     
-    Object.defineProperty(this, 'length', {
+    Object.defineProperty(this, 'magnitude', {
       get: function() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+        return Math.sqrt(sqrMagnitude(this));
+      }
+    });
+    
+    Object.defineProperty(this, 'sqrMagnitude', {
+      get: function() {
+        return sqrMagnitude(this);
       }
     });
     
@@ -17,24 +27,37 @@ define(function() {
     });
   };
   
-  Vector2.add = function(vector) {
+  Vector2.dot = function(vec1, vec2) {
+    return vec1.x * vec2.x + vec1.y * vec2.y;
+  };
+  
+  Vector2.prototype.clone = function() {
+    return new Vector2(this.x, this.y);
+  };
+  
+  Vector2.prototype.add = function(vector) {
     this.x += vector.x;
     this.y += vector.y;
   };
   
-  Vector2.subtract = function(vector) {
+  Vector2.prototype.subtract = function(vector) {
     this.x -= vector.x;
     this.y -= vector.y;
   };
   
-  Vector2.multiply = function(vector) {
+  Vector2.prototype.multiply = function(vector) {
     this.x *= vector.x;
     this.y *= vector.y;
   };
   
-  Vector2.divide = function(vector) {
+  Vector2.prototype.divide = function(vector) {
     this.x /= vector.x;
     this.y /= vector.y;
+  };
+  
+  Vector2.prototype.normalize = function() {
+    this.x = this.x / this.magnitude;
+    this.y = this.y / this.magnitude;
   };
   
   Vector2.fromAngle = function(angle, magnitude) {
