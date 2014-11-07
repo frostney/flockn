f9928740d97f638b3e584c861cb96dfd9ba3913c
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:39:44 +0100

    Vector2 module is now ES6

--- src/types/vector2.js
+++ src/types/vector2.js
@@ -1,69 +1,61 @@
-define(function() {
-  
-  var sqrMagnitude = function(v) {
-    return Vector2.dot(v, v);
-  };
-  
-  var Vector2 = function(x, y) {
-    this.x = this.x || 0;
-    this.y = this.y || 0;
-    
-    Object.defineProperty(this, 'magnitude', {
-      get: function() {
-        return Math.sqrt(sqrMagnitude(this));
-      }
-    });
-    
-    Object.defineProperty(this, 'sqrMagnitude', {
-      get: function() {
-        return sqrMagnitude(this);
-      }
-    });
-    
-    Object.defineProperty(this, 'angle', {
-      get: function() {
-        return Math.atan2(this.y, this.x);
-      }
-    });
-  };
-  
-  Vector2.dot = function(vec1, vec2) {
+let sqrMagnitude = function(v) {
+  return Vector2.dot(v, v);
+};
+
+class Vector2 {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  get magnitude() {
+    return Math.sqrt(sqrMagnitude(this));
+  }
+
+  get sqrMagnitude() {
+    return sqrMagnitude(this);
+  }
+
+  get angle() {
+    return Math.atan2(this.x, this.y);
+  }
+
+  static dot(vec1, vec2) {
     return vec1.x * vec2.x + vec1.y * vec2.y;
   };
-  
-  Vector2.prototype.clone = function() {
+
+  static fromAngle(angle, magnitude) {
+    return new Vector2(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
+  };
+
+  clone() {
     return new Vector2(this.x, this.y);
   };
-  
-  Vector2.prototype.add = function(vector) {
+
+  add(vector) {
     this.x += vector.x;
     this.y += vector.y;
   };
-  
-  Vector2.prototype.subtract = function(vector) {
+
+  subtract(vector) {
     this.x -= vector.x;
     this.y -= vector.y;
   };
-  
-  Vector2.prototype.multiply = function(vector) {
+
+  multiply(vector) {
     this.x *= vector.x;
     this.y *= vector.y;
   };
-  
-  Vector2.prototype.divide = function(vector) {
+
+  divide(vector) {
     this.x /= vector.x;
     this.y /= vector.y;
   };
-  
-  Vector2.prototype.normalize = function() {
+
+  normalize() {
     this.x = this.x / this.magnitude;
     this.y = this.y / this.magnitude;
   };
-  
-  Vector2.fromAngle = function(angle, magnitude) {
-    return new Vector2(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
-  };
-  
-  return Vector2;
-  
-});
+}
+
+export default Vector2;
