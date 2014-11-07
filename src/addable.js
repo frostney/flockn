3ac76cac3c0c5a4c2d4ab247bca6445dbfa45534
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:31:03 +0100

    addable module is now ES6

--- src/addable.js
+++ src/addable.js
@@ -1,49 +1,47 @@
-udefine(['./graphics'], function(Graphics) {
-  'use strict';
+import Graphics from 'flockn/graphics';
 
-  return function(Factory, groupInstance, extraFn) {
+var addable = function addable(Factory, groupInstance, extraFn) {
 
-    var adder = function() {
-      var child = arguments[0];
-      var args = 2 <= arguments.length ? [].slice.call(arguments, 1) : [];
+  var adder = function adder(child, ...args) {
 
-      if (!( child instanceof Factory)) {
-        if ( typeof child === 'string') {
-          if (Object.hasOwnProperty.call(Factory.store, child)) {
-            child = new Factory(Factory.store[child]);
-          }
+    if (!( child instanceof Factory)) {
+      if ( typeof child === 'string') {
+        if (Object.hasOwnProperty.call(Factory.store, child)) {
+          child = new Factory(Factory.store[child]);
+        }
+      } else {
+        if ( typeof child === 'function') {
+          child = new Factory(child);
         } else {
-          if ( typeof child === 'function') {
-            child = new Factory(child);
-          } else {
-            // TODO: This should be also able to deep assign properties
-            child = new Factory(function() {
-              Object.keys(child).forEach(function(key) {
-                this[key] = child[key];
-              }, this);
-            });
-          }
+          // TODO: This should be also able to deep assign properties
+          child = new Factory(function() {
+            Object.keys(child).forEach(function(key) {
+              this[key] = child[key];
+            }, this);
+          });
         }
       }
-      groupInstance.push(child);
-      child.parent = this;
+    }
+    groupInstance.push(child);
+    child.parent = this;
 
-      if (extraFn) {
-        extraFn.call(this, child);
-      }
+    if (extraFn) {
+      extraFn.call(this, child);
+    }
 
-      Graphics.trigger('add', child);
+    Graphics.trigger('add', child);
 
-      child.apply(args);
-      child.trigger('add', child, args);
-    };
-
-    return function() {
-      var args = [].slice.call(arguments);
-      args.unshift(this);
-
-      return adder.bind.apply(adder, args);
-    };
-
+    child.apply(args);
+    child.trigger('add', child, args);
   };
-});
+
+  return function() {
+    var args = [].slice.call(arguments);
+    args.unshift(this);
+
+    return adder.bind.apply(adder, args);
+  };
+
+};
+
+export default addable;
