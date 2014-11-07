a6266c5efa0a68d7338d99b12f3e4970f4faa90d
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:37:58 +0100

    renderable module is now ES6

--- src/renderable.js
+++ src/renderable.js
@@ -1,17 +1,15 @@
-udefine(['./graphics'], function(Graphics) {
-  'use strict';
+import Graphics from 'flockn/graphics';
 
-  return function() {
-    var self = this;
+var renderable = function renderable() {
+  this.on('render', () => {
+    // Emit `render` event on the `Graphics` object
+    Graphics.trigger('render', this);
 
-    this.on('render', function() {
-      // Emit `render` event on the `Graphics` object
-      Graphics.trigger('render', self);
-
-      // Render all children elements
-      self.children.forEach(function(child) {
-        child.trigger('render');
-      });
+    // Render all children elements
+    self.children.forEach(function(child) {
+      child.trigger('render');
     });
-  };
-});
+  });
+};
+
+export default renderable;
