1b98f20f152a6a627e6d6e8962406046ea8400d3
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:35:30 +0100

    GameObject module is now ES6

--- src/gameobject.js
+++ src/gameobject.js
@@ -1,10 +1,18 @@
-udefine(['mixedice', './addable', './base', './behavior', './graphics', './group', './model', './renderable', './serialize', './texture', './updateable'], function(mixedice, addable, Base, Behavior, Graphics, Group, Model, renderable, serialize, Texture, updateable) {
-  'use strict';
+import addable from 'flokn/addable';
+import Base from 'flockn/base';
+import Behavior from 'flockn/behavior';
+import Graphics from 'flockn/graphics';
+import Group from 'flockn/group';
+import Model from 'flockn/model';
+import renderable from 'flockn/renderable';
+import serialize from 'flockn/serialize';
+import Texture from 'flockn/texture';
+import updateable from 'flockn/updateable';
 
-  var GameObject = function(descriptor) {
+
+class GameObject {
+  constructor(descriptor) {
     Base.extend([this, GameObject.prototype], 'GameObject', descriptor);
-
-    var self = this;
 
     this.visible = true;
 
@@ -12,77 +20,37 @@ udefine(['mixedice', './addable', './base', './behavior', './graphics', './group
     this.y = 0;
     this.z = 0;
 
-    Object.defineProperty(this, 'left', {
-      get: function() {
-        return this.x;
-      },
-      set: function(value) {
-        this.x = value;
-      },
-      enumerable: true
-    });
-
-    Object.defineProperty(this, 'top', {
-      get: function() {
-        return this.y;
-      },
-      set: function(value) {
-        this.y = value;
-      },
-      enumerable: true
-    });
-
-    Object.defineProperty(this, 'right', {
-      get: function() {
-        return this.parent.width - this.width - this.x;
-      },
-      set: function(value) {
-        this.x = this.parent.width - this.width - value;
-      },
-      enumerable: true
-    });
-
-    Object.defineProperty(this, 'bottom', {
-      get: function() {
-        return this.parent.height - this.height - this.y;
-      },
-      set: function(value) {
-        this.y = this.parent.height - this.height - value;
-      },
-      enumerable: true
-    });
-
     this.fitToTexture = true;
 
     // Create a new texture and bind it to the `texture` property
     this.texture = new Texture();
     this.texture.parent = this;
-    
-    // Once the image is loaded, update width and height if `fitToTexture` is set
-    this.texture.on('image-loaded', function() {
-      if (self.fitToTexture) {
-        self.width = self.texture.image.width;
-        self.height = self.texture.image.height;
 
-        self.origin.x = (self.width / 2);
-        self.origin.y = (self.height / 2);
+    // Once the image is loaded, update width and height if `fitToTexture` is set
+    this.texture.on('image-loaded', () => {
+      if (this.fitToTexture) {
+        this.width = this.texture.image.width;
+        this.height = this.texture.image.height;
+
+        this.origin.x = (this.width / 2);
+        this.origin.y = (this.height / 2);
       }
 
       // TODO: Evaluate if the Graphics trigger should only be in the texture
-      Graphics.trigger('texture-image-loaded', self, self.texture);
+      Graphics.trigger('texture-image-loaded', this, this.texture);
     });
 
     // Once the label is loaded, update width and height if `fitToTexture` is set
-    this.texture.on('label-loaded', function() {
-      if (self.fitToTexture) {
-        self.width = self.texture.label.width;
-        self.height = self.texture.label.height;
+    this.texture.on('label-loaded', () => {
+      if (this.fitToTexture) {
+        this.width = this.texture.label.width;
+        this.height = this.texture.label.height;
 
-        self.origin.x = (self.width / 2);
-        self.origin.y = (self.height / 2);
+        this.origin.x = (this.width / 2);
+        this.origin.y = (this.height / 2);
 
         // TODO: Evaluate if the Graphics trigger should only be in the texture
-        Graphics.trigger('texture-label-loaded', self, self.texture);
+        Graphics.trigger('texture-label-loaded', this, this.texture);
       }
     });
 
@@ -99,8 +67,8 @@ udefine(['mixedice', './addable', './base', './behavior', './graphics', './group
     };
 
     this.origin = {
-      x: (self.width / 2),
-      y: (self.width / 2)
+      x: (this.width / 2),
+      y: (this.width / 2)
     };
 
     this.border = {
@@ -120,55 +88,80 @@ udefine(['mixedice', './addable', './base', './behavior', './graphics', './group
     updateable.call(this);
 
     // Update all behaviors as well
-    this.on('update', function() {
-      self.behaviors.forEach(function(behavior) {
+    this.on('update', () => {
+      this.behaviors.forEach(function(behavior) {
         behavior.trigger('update');
       });
     });
-  };
+  }
 
-  GameObject.store = {};
+  get left() {
+    return this.x;
+  }
 
-  // Game objects can be defined and are stored on the object itself
-  GameObject.define = function(name, factory) {
-    GameObject.store[name] = factory;
-  };
+  set left(value) {
+    this.x = value;
+  }
 
-  GameObject.prototype.addGameObject = function() {
+  get top() {
+    return this.y;
+  }
+
+  set top(value) {
+    this.y = value;
+  }
+
+  get right() {
+    return this.parent.width - this.width - this.x;
+  }
+
+  set right(value) {
+    this.x = this.parent.width - this.width - value;
+  }
+
+  get bottom() {
+    return this.parent.height - this.height - this.y;
+  }
+
+  set bottom(value) {
+    this.y = this.parent.height - this.height - value;
+  }
+
+  addGameObject() {
     // Add a game object to this game object
     this.queue.push(addable(GameObject, this.children).apply(this, arguments));
-  };
+  }
 
-  GameObject.prototype.addBehavior = function() {
+  addBehavior() {
     // Add a `Behavior` instance to the the game object and update the `gameObject` property
     this.queue.push(addable(Behavior, this.behaviors, function(child) {
       child.gameObject = this;
     }).apply(this, arguments));
-  };
+  }
 
-  GameObject.prototype.addModel = function() {
+  addModel() {
     // Add a `Model` instance to the game object
     this.queue.push(addable(Model, this.models).apply(this, arguments));
-  };
-  
-  GameObject.prototype.removeGameObject = function() {
-    
-  };
-  
-  GameObject.prototype.removeBehavior = function() {
-    
-  };
+  }
 
-  GameObject.prototype.toJSON = function() {
+  removeGameObject() {
+
+  }
+
+  removeBehavior() {
+
+  }
+
+  removeModel() {
+
+  }
+
+  toJSON() {
     // Serialize this object
     return serialize(this);
-  };
+  }
 
-  GameObject.prototype.fromJSON = function() {
-
-  };
-
-  GameObject.prototype.animate = function(property, end, time, callback) {
+  animate(property, end, time, callback) {
     // TODO: Tweening does not work yet
     if ( typeof this[property] === 'number') {
       var distance = end - this[property];
@@ -180,7 +173,18 @@ udefine(['mixedice', './addable', './base', './behavior', './graphics', './group
         this.off(animateName);
       });
     }
-  };
+  }
 
-  return GameObject;
-});
+  // Game objects can be defined and are stored on the object itself
+  static define(name, factory) {
+    GameObject.store[name] = factory;
+  }
+
+  static fromJSON() {
+
+  }
+}
+
+GameObject.store = {};
+
+export default GameObject;
