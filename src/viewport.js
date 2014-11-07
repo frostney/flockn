d27fb87248ee6bd857c71e669b29c8a2d91fffe4
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:40:21 +0100

    Viewport module is now ES6

--- src/viewport.js
+++ src/viewport.js
@@ -1,13 +1,11 @@
-define(['mixedice', 'eventmap'], function(mixedice, EventMap) {
-  var Viewport = {};
+var Viewport = {};
 
-  Viewport.scale = {};
-  Viewport.scale.mode = 'scaleToFit';
-  Viewport.scale.x = 1.0;
-  Viewport.scale.y = 1.0;
+Viewport.scale = {};
+Viewport.scale.mode = 'scaleToFit';
+Viewport.scale.x = 1.0;
+Viewport.scale.y = 1.0;
 
-  Viewport.width = 800;
-  Viewport.height = 600;
+Viewport.width = 800;
+Viewport.height = 600;
 
-  return Viewport;
-}); 
+export default Viewport;
