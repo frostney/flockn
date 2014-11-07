01705c8dd29f0f568f7105db1cc5257811509302
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:37:31 +0100

    Model module is now ES6

--- src/model.js
+++ src/model.js
@@ -1,28 +1,26 @@
-udefine(['mixedice', 'eventmap'], function(mixedice, EventMap) {
-  'use strict';
+import * as mixedice from 'mixedice';
+import * as EventMap from 'eventmap';
 
-  var Model = function() {
+class Model {
+  constructor() {
     // Mix in `EventMap` into all `Model` instances
     mixedice([this, Model.prototype], new EventMap());
-    
+
     // Store attribute data
     this.data = {};
-  };
+  }
 
-  Model.prototype.get = function(name) {
+  get() {
     // Get an attribute if it exists
     if (Object.hasOwnProperty.call(this.data, name)) {
       return this.data[name];
     }
-  };
+  }
 
-  Model.prototype.set = function(name, value) {
+  set(name, value) {
     // Set or add an attribute
     this.data[name] = value;
     // Trigger the `change` event with `name` and `value` as its parameters
     this.trigger('change', name, value);
-  };
-
-  return Model;
-
-});
+  }
+}
