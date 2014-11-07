a4dac7958f5cc431554b7617fec8d8c2bd08bf39
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:38:45 +0100

    serialize module is now ES6

--- src/serialize.js
+++ src/serialize.js
@@ -1,20 +1,18 @@
-udefine(function() {
-  'use strict';
+// Serialize function to `JSON.stringify` with a custom replacer
+var serialize = function serialize(obj) {
+  return JSON.stringify(obj, function(key, value) {
+    // Avoiding cyclic dependencies
+    if (key === 'parent') {
+      return;
+    }
 
-  // Serialize function to `JSON.stringify` with a custom replacer
-  return function(obj) {
-    return JSON.stringify(obj, function(key, value) {
-      // Avoiding cyclic dependencies
-      if (key === 'parent') {
-        return;
-      }
+    // Stringify the descriptor
+    if (key === 'descriptor') {
+      value = value.toString();
+    }
 
-      // Stringify the descriptor
-      if (key === 'descriptor') {
-        value = value.toString();
-      }
+    return value;
+  });
+};
 
-      return value;
-    });
-  };
-});
+export default serialize;
