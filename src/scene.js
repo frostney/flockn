25f4e4dd3bbf609191c98bc78750ce842189ddc5
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-07 23:38:29 +0100

    Scene module is now ES6

--- src/scene.js
+++ src/scene.js
@@ -1,29 +1,30 @@
-udefine(['mixedice', './addable', './base', './group', './gameobject', './renderable', './updateable'], function(mixedice, addable, Base, Group, GameObject, renderable, updateable) {
-  'use strict';
+import addable from 'flockn/addable';
+import Base from 'flockn/base';
+import GameObject from 'flockn/gameobject';
+import renderable from 'flockn/renderable';
+import updateable from 'flockn/updateable';
 
-  // A `Scene` instance is a layer for `GameObject` instances.
-  // Any number of game objects can be added to a scene. Only one scene should be visible at the same time, depending
-  // on what was set in the `activeScene` property of a `Game` instance.
-  var Scene = function(descriptor) {
-    Base.extend([this, Scene.prototype], 'Scene', descriptor);
+// A `Scene` instance is a layer for `GameObject` instances.
+// Any number of game objects can be added to a scene. Only one scene should be visible at the same time, depending
+// on what was set in the `activeScene` property of a `Game` instance.
+var Scene = function(descriptor) {
+  Base.extend([this, Scene.prototype], 'Scene', descriptor);
 
-    // Mix in `renderable` and `updateable`
-    renderable.call(this);
-    updateable.call(this);
-  };
+  // Mix in `renderable` and `updateable`
+  renderable.call(this);
+  updateable.call(this);
+};
 
-  Scene.prototype.addGameObject = function() {
-    // Allow game objects to be added to scenes
-    this.queue.push(addable(GameObject, this.children).apply(this, arguments));
-  };
+Scene.prototype.addGameObject = function() {
+  // Allow game objects to be added to scenes
+  this.queue.push(addable(GameObject, this.children).apply(this, arguments));
+};
 
-  Scene.store = {};
+Scene.store = {};
 
-  // Scenes can be defined and are stored on the object itself
-  Scene.define = function(name, factory) {
-    Scene.store[name] = factory;
-  };
+// Scenes can be defined and are stored on the object itself
+Scene.define = function(name, factory) {
+  Scene.store[name] = factory;
+};
 
-  return Scene;
-
-});
+export default Scene;
