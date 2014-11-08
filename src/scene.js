132b2f1dcaef22bf551b7c772fb425cc361a44da
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-08 13:28:11 +0100

    Using ES6 inheritence in scene module

--- src/scene.js
+++ src/scene.js
@@ -7,24 +7,24 @@ import updateable from 'flockn/updateable';
 // A `Scene` instance is a layer for `GameObject` instances.
 // Any number of game objects can be added to a scene. Only one scene should be visible at the same time, depending
 // on what was set in the `activeScene` property of a `Game` instance.
-var Scene = function(descriptor) {
-  Base.extend([this, Scene.prototype], 'Scene', descriptor);
+class Scene extends Base {
+  constructor(descriptor) {
+    super('Scene', descriptor);
 
-  // Mix in `renderable` and `updateable`
-  renderable.call(this);
-  updateable.call(this);
-};
+    // Mix in `renderable` and `updateable`
+    renderable.call(this);
+    updateable.call(this);
+  }
 
-Scene.prototype.addGameObject = function() {
-  // Allow game objects to be added to scenes
-  this.queue.push(addable(GameObject, this.children).apply(this, arguments));
-};
+  addGameObject() {
+    // Allow game objects to be added to scenes
+    this.queue.push(addable(GameObject, this.children).apply(this, arguments));
+  }
 
-Scene.store = {};
-
-// Scenes can be defined and are stored on the object itself
-Scene.define = function(name, factory) {
-  Scene.store[name] = factory;
-};
+  // Scenes can be defined and are stored on the object itself
+  static define(name, factory) {
+    Scene.store[name] = factory;
+  }
+}
 
 export default Scene;
