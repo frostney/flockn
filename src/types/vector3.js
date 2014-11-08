6ca5813c34685b1a76c8d638fd9c82c70082f171
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-08 16:07:06 +0100

    Add set method to Vector3

--- src/types/vector3.js
+++ src/types/vector3.js
@@ -4,6 +4,10 @@ let sqrMagnitude = function(v) {
 
 class Vector3 {
   constructor(x = 0, y = 0, z = 0) {
+    this.set(x, y, z);
+  }
+
+  set(x = 0, y = 0, z = 0) {
     this.x = x;
     this.y = y;
     this.z = z;
