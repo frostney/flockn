38d5cd6f50e61eced4f59166dded48995dc4b1d0
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-08 15:14:21 +0100

    Removed semicolons

--- src/types/vector2.js
+++ src/types/vector2.js
@@ -22,40 +22,40 @@ class Vector2 {
 
   static dot(vec1, vec2) {
     return vec1.x * vec2.x + vec1.y * vec2.y;
-  };
+  }
 
   static fromAngle(angle, magnitude) {
     return new Vector2(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
-  };
+  }
 
   clone() {
     return new Vector2(this.x, this.y);
-  };
+  }
 
   add(vector) {
     this.x += vector.x;
     this.y += vector.y;
-  };
+  }
 
   subtract(vector) {
     this.x -= vector.x;
     this.y -= vector.y;
-  };
+  }
 
   multiply(vector) {
     this.x *= vector.x;
     this.y *= vector.y;
-  };
+  }
 
   divide(vector) {
     this.x /= vector.x;
     this.y /= vector.y;
-  };
+  }
 
   normalize() {
     this.x = this.x / this.magnitude;
     this.y = this.y / this.magnitude;
-  };
+  }
 }
 
 export default Vector2;
