cfa22d2c1cbdc40460a01272b40084c502f15e1c
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-08 14:12:35 +0100

    Mixin structure container

---
+++ src/mixins/index.js
@@ -0,0 +1,5 @@
+import addable from 'flockn/mixins/addable';
+import renderable from 'flockn/mixins/renderable';
+import updateable from 'flockn/mixins/updateable';
+
+export {addable, renderable, updateable};
