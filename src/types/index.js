bbfa51044204f99aac53e6469a511018b3e0d3f8
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-08 15:26:58 +0100

    Update types container to include Vector3

--- src/types/index.js
+++ src/types/index.js
@@ -1,10 +1,12 @@
 import Color from 'flockn/types/color';
 import Vector2 from 'flockn/types/vector2';
+import Vector3 from 'flockn/types/vector3';
 
 let Types = {};
 
 Types.Color = Color;
 Types.Vector2 = Vector2;
+Types.Vector3 = Vector3;
 
 export default Types;
-export {Color, Vector2};
+export {Color, Vector2, Vector3};
