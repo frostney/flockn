1683f1b1faad40552d4e52420602dca8d9786774
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-23 00:57:04 +0100

    Ducktyping for updatable events

--- src/mixins/updateable.js
+++ src/mixins/updateable.js
@@ -2,9 +2,11 @@ var updatable = function() {
   var self = this;
 
   // Update all children
-  this.on('update', function(dt) {
-    self.children.forEach(function(child) {
-      child.trigger('update', dt);
+  this.on('update', dt => {
+    self.children.forEach(child => {
+      if (child.update) {
+        child.trigger('update', dt);
+      }
     });
   });
 };
