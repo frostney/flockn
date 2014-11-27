abc6c2d42c1f5d7063974ac0bd196bbf05b3205a
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-27 18:32:14 +0100

    Add an "visible" equivalent for update mixin

--- src/mixins/updateable.js
+++ src/mixins/updateable.js
@@ -1,9 +1,16 @@
-var updatable = function() {
-  var self = this;
+import checkForFlag from 'flockn/utils/ckeckforflag';
 
+var isStatic = checkForFlag('static');
+
+// TODO: This is not completely how I want it be as it only sets the children as static and not the element itself
+var updatable = function updateable() {
   // Update all children
   this.on('update', dt => {
-    self.children.forEach(child => {
+    if (!isStatic.call(this)) {
+      return;
+    }
+
+    this.children.forEach(child => {
       if (child.update) {
         child.trigger('update', dt);
       }
