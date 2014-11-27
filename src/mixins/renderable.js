81237676124a42c04a6129dcb0af1ce4b555faf9
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-27 08:35:49 +0100

    Emphasis on visible flag being optional

--- src/mixins/renderable.js
+++ src/mixins/renderable.js
@@ -2,13 +2,18 @@ import Graphics from 'flockn/graphics';
 
 var isVisible = function isVisible() {
   var hasVisibleFlag = Object.hasOwnProperty.call(this, 'visible');
-  return hasVisibleFlag && this.visible;
+
+  if (hasVisibleFlag) {
+    return this.visible;
+  } else {
+    return true;
+  }
 };
 
 var renderable = function renderable() {
   this.on('render', () => {
     // Only render if element is visible
-    if (!isVisible()) {
+    if (!isVisible.call(this)) {
       return;
     }
 
