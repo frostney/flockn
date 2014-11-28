af9dd9ad518687545ef841466db2f6dad3661d17
Johannes Stein <johannesstein@freeze-dev.com>
2014-11-28 23:09:10 +0100

    Generialize serialize method

--- src/serialize.js
+++ src/serialize.js
@@ -1,9 +1,40 @@
+import {Color, Vector2, Vector3, Rect} from 'flockn/types';
+
 // Serialize function to `JSON.stringify` with a custom replacer
 var serialize = function serialize(obj) {
   return JSON.stringify(obj, function(key, value) {
     // Avoiding cyclic dependencies
     if (key === 'parent') {
       return;
+    }
+
+    if (value instanceof Color) {
+      value = value.toString();
+    }
+
+    if (value instanceof Vector2) {
+      value = value.toString();
+    }
+
+    if (value instanceof Vector3) {
+      value = value.toString();
+    }
+
+    if (value instanceof Rect) {
+      value = value.toString();
+    }
+
+    // Convert image to Base64
+    if (value instanceof Image) {
+      let canvas = document.createElement('canvas');
+      let context = canvas.getContext('2d');
+      canvas.height = this.height;
+      canvas.width = this.width;
+      context.drawImage(this.data, 0, 0);
+      let dataURL = canvas.toDataURL('image/png');
+      canvas = null;
+
+      value = dataURL;
     }
 
     // Stringify the descriptor
