7d28f11effab5cdab3b0322aefc3a12f63b08a53
Johannes Stein <johannesstein@freeze-dev.com>
2014-12-03 22:50:43 +0100

    Update mixin container

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,5 +1,6 @@
 import addable from 'flockn/mixins/addable';
 import renderable from 'flockn/mixins/renderable';
 import updateable from 'flockn/mixins/updateable';
+import serializable from 'flockn/mixins/serializable';
 
-export {addable, renderable, updateable};
+export {addable, renderable, updateable, serializable};
