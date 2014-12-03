05a906a4002923c2f8de76222277da8f885e9f57
Johannes Stein <johannesstein@freeze-dev.com>
2014-12-03 22:36:38 +0100

    Serialization is now more generic

--- src/serialize.js
+++ src/serialize.js
@@ -1,61 +1,92 @@
 import EventMap from 'eventmap';
 
-// Serialize function to `JSON.stringify` with a custom replacer
-var serialize = function serialize(obj) {
-  // Shift parent reference
-  // TODO: This is a bit a hacky I understand, but it works for now. Once the object is passed into
-  //  JSON.stringify, we ran into a call stack error
-  var parentRef = null;
+var serialize = {};
 
-  if (Object.hasOwnProperty.call(obj, 'parent')) {
-    parentRef = obj.parent;
-    obj.parent = null;
+serialize.json = {};
+
+serialize.json.filter = ['id', 'parent'];
+serialize.json.defaultReplacer = [];
+
+serialize.json.defaultReplacer.push(function(key, value) {
+  if (key === 'events' && obj instanceof EventMap) {
+    value = obj.serialize();
   }
 
-  var string = JSON.stringify(obj, function(key, value) {
-    console.log(key, value);
+  return value;
+});
 
-    // Avoiding cyclic dependencies
-    if (key === 'parent' || key === 'world' || key === 'input') {
-      return;
-    }
+serialize.json.defaultReplacer.push(function(key, value) {
+  // Convert image to Base64
+  if (value instanceof Image) {
+    let canvas = document.createElement('canvas');
+    let context = canvas.getContext('2d');
+    canvas.height = this.height;
+    canvas.width = this.width;
+    context.drawImage(this.data, 0, 0);
+    let dataURL = canvas.toDataURL('image/png');
+    canvas = null;
 
-    if (key === 'events' && obj instanceof EventMap) {
-      value = obj.serialize();
-    }
-
-    // Use custom toString function if available
-    if (typeof value === 'object' && value != null && Object.hasOwnProperty.call(value, 'toString')) {
-      value = value.toString();
-    }
-
-    // Convert image to Base64
-    if (value instanceof Image) {
-      let canvas = document.createElement('canvas');
-      let context = canvas.getContext('2d');
-      canvas.height = this.height;
-      canvas.width = this.width;
-      context.drawImage(this.data, 0, 0);
-      let dataURL = canvas.toDataURL('image/png');
-      canvas = null;
-
-      value = dataURL;
-    }
-
-    // Stringify the descriptor
-    if (key === 'descriptor') {
-      value = value.toString();
-    }
-
-    return value;
-  });
-
-  // Put the parent reference back in
-  if (Object.hasOwnProperty.call(obj, 'parent')) {
-    obj.parent = parentRef
+    value = {
+      data: dataURL,
+      type: 'image/png'
+    };
   }
 
-  return string;
+  return value;
+});
+
+serialize.json.defaultReplacer.push(function(key, value) {
+  // Stringify the descriptor
+  if (key === 'descriptor') {
+    value = value.toString();
+  }
+
+  return value;
+});
+
+serialize.json.defaultReplacer.push(function(key, value) {
+  if (Object.hasOwnProperty(value, 'toJSON')) {
+    value = value.toJSON();
+  }
+
+  return value;
+});
+
+serialize.toJSON = function(obj, replacer) {
+  var clonedObj = {};
+
+  var replacers = [].concat.apply([], [serialize.json.defaultReplacer, replacer]);
+  
+
+  for (var key in obj) {
+    (function(key, value) {
+      if (!Object.hasOwnProperty.call(obj, key)) {
+        return;
+      }
+
+      if (serialize.json.filter.indexOf(key) >= 0) {
+        return;
+      }
+
+      for (var i = 0, j = replacers.length; i < j; i++) {
+        (function(rep) {
+          if (rep) {
+            value = rep.call(obj, key, value);
+          }
+        })(replacers[i]);
+      }
+
+      if (typeof value !== 'undefined') {
+        clonedObj[key] = value;
+      }
+    })(key, obj[key]);
+  }
+
+  return clonedObj;
+};
+
+serialize.toString = function(obj) {
+  return JSON.stringify(serialize.toJSON(obj));
 };
 
 export default serialize;
