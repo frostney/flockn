cf6309a0f8f5cfbef282ebde962aada0c0606c29
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-13 00:09:03 +0100

    Update test file

--- test/spec/base.js
+++ test/spec/base.js
@@ -1,23 +1,20 @@
-define('spec/base', ['flockn/base'], function(Base) {
-  'use strict';
-  
-  describe('flockn/base', function() {
-    
-    it('is a function', function() {
-      expect(Base).to.be.a('function');
-    });
-    
-    describe('constructor', function() {
-      it('can be instantiated', function() {
-        var base = new Base();
+'use strict';
 
-        expect(base).to.be.a('object');
-        expect(base).to.be.an.instanceOf(Base);
-      });
-      
-    });
-    
-    
+import Base from 'flockn/base';
+
+describe('flockn/base', function () {
+
+  it('is a function', function () {
+    expect(Base).to.be.a('function');
   });
-  
+
+  describe('constructor', function () {
+    it('can be instantiated', function () {
+      var base = new Base();
+
+      expect(base).to.be.a('object');
+      expect(base).to.be.an.instanceOf(Base);
+    });
+
+  });
 });
