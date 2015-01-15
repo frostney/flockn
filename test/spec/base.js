6a300a4200cef6aa60e20a7ff56c56035fc9001a
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-15 08:31:38 +0100

    Remove everything from test/spec/*

--- test/spec/base.js
+++ test/spec/base.js
@@ -1,20 +1,24 @@
-'use strict';
+define('spec/base', ["exports", "flockn/base"], function (exports, _flocknBase) {
+  "use strict";
 
-import Base from 'flockn/base';
+  var _interopRequire = function (obj) {
+    return obj && (obj["default"] || obj);
+  };
 
-describe('flockn/base', function () {
+  var Base = _interopRequire(_flocknBase);
 
-  it('is a function', function () {
-    expect(Base).to.be.a('function');
-  });
-
-  describe('constructor', function () {
-    it('can be instantiated', function () {
-      var base = new Base();
-
-      expect(base).to.be.a('object');
-      expect(base).to.be.an.instanceOf(Base);
+  describe("flockn/base", function () {
+    it("is a function", function () {
+      expect(Base).to.be.a("function");
     });
 
+    describe("constructor", function () {
+      it("can be instantiated", function () {
+        var base = new Base();
+
+        expect(base).to.be.a("object");
+        expect(base).to.be.an.instanceOf(Base);
+      });
+    });
   });
 });
