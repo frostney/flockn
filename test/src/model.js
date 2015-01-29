bf30851fc9bd062ca11ce24b58ab15d7d2fbacfb
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-29 22:35:11 +0100

    Add model test

---
+++ test/src/model.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import EventMap from 'eventmap';
+import Model from 'flockn/model';
+
+describe('flockn/model', function () {
+
+  it('is a function', function () {
+    expect(Model).to.be.a('function');
+  });
+
+  describe('constructor', function () {
+    it('can be instantiated', function () {
+      var model = new Model();
+
+      expect(model).to.be.a('object');
+      expect(model).to.be.an.instanceOf(Game);
+    });
+
+    it('inherits from EventMap', function() {
+      var model = new Model();
+
+      expect(model).to.be.an.instanceOf(EventMap);
+    });
+  });
+
+  describe('#get', function() {
+    it('undefined when there is no valid property', function() {
+      var model = new Model();
+
+      expect(model.get('text')).to.be(undefined);
+    });
+
+    it('exact property value', function() {
+      var model = new Model();
+      model.set('test', 5);
+
+      expect(model.get('test')).to.be(5);
+    });
+  });
+
+});
