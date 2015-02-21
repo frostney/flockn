6ee945b7c8c0a9d89d6cb120560c6106317ac29f
Johannes Stein <johannesstein@freeze-dev.com>
2015-02-21 12:40:43 +0100

    More helper functions for the flockn module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import Game from 'flockn/game';
 
-export default function(descriptor) {
+import GameObject from 'flockn/gameobject';
+import Scene from 'flockn/scene';
+import Behavior from 'flockn/behavior';
+
+var flockn = function flockn(descriptor) {
   return new Game(descriptor);
-}
+};
+
+flockn.gameObject = function(name, factory) {
+  return GameObject.define(name, factory);
+};
+
+flockn.scene = function(name, factory) {
+  return Scene.define(name, factory);
+};
+
+flockn.behavior = function(name, factory) {
+  return Behavior.define(name, factory);
+};
+
+export default flockn;
