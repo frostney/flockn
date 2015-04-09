9e41bfaf2c5ff453d699287e1879b18c6a00a225
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-09 19:43:56 +0200

    :fire: Remove storable from mixin index

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -2,6 +2,5 @@ import addable from 'flockn/mixins/addable';
 import renderable from 'flockn/mixins/renderable';
 import updateable from 'flockn/mixins/updateable';
 import serializable from 'flockn/mixins/serializable';
-import storable from 'flockn/mixins/storable';
 
-export {addable, renderable, updateable, serializable, storable};
+export {addable, renderable, updateable, serializable};
