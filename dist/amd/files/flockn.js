eb13b7d9df90987bbe5b4f66fb570895f65dba45
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-12 22:09:44 +0200

    Update dist files

--- dist/amd/files/flockn.js
+++ dist/amd/files/flockn.js
@@ -1075,55 +1075,55 @@ define('flockn/input/mouse', ['exports', 'flockn/types/vector2'], function (expo
 });
 
 define('flockn/mixins/addable', ['exports', 'module', 'flockn/graphics'], function (exports, module, _flocknGraphics) {
-  'use strict';
+    'use strict';
 
-  var _interopRequire = function (obj) { return obj && obj.__esModule ? obj['default'] : obj; };
+    var _interopRequire = function (obj) { return obj && obj.__esModule ? obj['default'] : obj; };
 
-  var _Graphics = _interopRequire(_flocknGraphics);
+    var _Graphics = _interopRequire(_flocknGraphics);
 
-  var addable = function addable(Factory, groupInstance, extraFn) {
+    var addable = function addable(Factory, groupInstance, extraFn) {
 
-    var adder = function adder(child) {
-      for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-        args[_key - 1] = arguments[_key];
-      }
+        var adder = function adder(child) {
+            for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+                args[_key - 1] = arguments[_key];
+            }
 
-      // I have decided against letting anything other through than functions
-      // I feel that it more complexity than it tried to solve and I had to handle some edge cases
-      // and more thorough type checking
+            // I have decided against letting anything other through than functions
+            // I feel that it more complexity than it tried to solve and I had to handle some edge cases
+            // and more thorough type checking
 
-      if (typeof child !== 'function') {
-        throw new Error('A child has to be a function');
-      }
+            if (typeof child !== 'function') {
+                throw new Error('A child has to be a function');
+            }
 
-      child = new Factory(child);
+            child = new Factory(child);
 
-      groupInstance.push(child);
-      child.parent = this;
+            groupInstance.push(child);
+            child.parent = this;
 
-      if (extraFn) {
-        extraFn.call(this, child);
-      }
+            if (extraFn) {
+                extraFn.call(this, child);
+            }
 
-      _Graphics.trigger('add', child);
+            _Graphics.trigger('add', child);
 
-      // Only call apply if it's available. Models for example don't have one
-      if (child.apply) {
-        child.apply(args);
-      }
+            // Only call apply if it's available. Models for example don't have one
+            if (child.apply) {
+                child.apply(args);
+            }
 
-      child.trigger('add', child, args);
+            child.trigger('add', child, args);
+        };
+
+        return function () {
+            var args = [].slice.call(arguments);
+            args.unshift(this);
+
+            return adder.bind.apply(adder, args);
+        };
     };
 
-    return function () {
-      var args = [].slice.call(arguments);
-      args.unshift(this);
-
-      return adder.bind.apply(adder, args);
-    };
-  };
-
-  module.exports = addable;
+    module.exports = addable;
 });
 
 define('flockn/mixins', ['exports', 'flockn/mixins/addable', 'flockn/mixins/renderable', 'flockn/mixins/updateable', 'flockn/mixins/serializable'], function (exports, _flocknMixinsAddable, _flocknMixinsRenderable, _flocknMixinsUpdateable, _flocknMixinsSerializable) {
