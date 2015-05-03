ace8935d67b600d6fc909aad1f8e69211e346b43
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-03 22:37:54 +0200

    Setting renderer is more explicit now

---
+++ src/renderer/index.js
@@ -0,0 +1,21 @@
+import {DOMRenderer} from './dom';
+import {CanvasRenderer} from './canvas';
+
+let renderers = {};
+
+let Renderer = {};
+
+Renderer.register = function(name, descriptor) {
+  renderers[name] = descriptor;
+};
+
+Renderer.use = function(name) {
+  if (Object.hasOwnProperty.call(renderers, name)) {
+    renderers[name]();
+  }
+};
+
+Renderer.register('dom', DOMRenderer);
+Renderer.register('canvas', CanvasRenderer);
+
+export default Renderer;
