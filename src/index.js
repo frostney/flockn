c7c203df1403fcb38e51c1e5c5d75604a4727744
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-03 22:41:32 +0200

    Remove definable objects

--- src/index.js
+++ src/index.js
@@ -8,16 +8,6 @@ var flockn = function flockn(descriptor) {
   return new Game(descriptor);
 };
 
-flockn.gameObject = function(name, factory) {
-  return GameObject.define(name, factory);
-};
-
-flockn.scene = function(name, factory) {
-  return Scene.define(name, factory);
-};
-
-flockn.behavior = function(name, factory) {
-  return Behavior.define(name, factory);
 };
 
 export default flockn;
