7218cfe8ec908037684665e4c374bdd7d299e931
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-03 21:56:31 +0200

    :fire: Use gamebox color and add color constants

--- src/types/color.js
+++ src/types/color.js
@@ -1,85 +1,9 @@
-import {Math} from 'gamebox';
-import colorConstants from 'flockn/constants/color';
+import {Math, Types} from 'gamebox';
+import colorConstants from '../constants/color';
+
+let {Color} = Types;
 
 const {clamp} = Math;
-
-class Color {
-  constructor(r = 0, g = 0, b = 0, a = 1) {
-    this.set(r, g, b, a);
-  }
-
-  set(r = 0, g = 0, b = 0, a = 1) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-    this.a = a;
-  }
-
-  lighten(factor) {
-    factor = clamp(factor, 0, 1);
-
-    this.r = clamp(this.r + (factor * 255) | 0, 0, 255);
-    this.g = clamp(this.g + (factor * 255) | 0, 0, 255);
-    this.b = clamp(this.b + (factor * 255) | 0, 0, 255);
-  }
-
-  darken(factor) {
-    factor = clamp(factor, 0, 1);
-
-    this.r = clamp(this.r - (factor * 255) | 0, 0, 255);
-    this.g = clamp(this.g - (factor * 255) | 0, 0, 255);
-    this.b = clamp(this.b - (factor * 255) | 0, 0, 255);
-  }
-
-  fadeIn(factor) {
-    factor = clamp(factor, 0, 1);
-
-    this.a = this.a + this.a * factor;
-    if (this.a > 1) {
-      this.a = 1;
-    }
-  }
-
-  fadeOut(factor) {
-    factor = clamp(factor, 0, 1);
-
-    this.a = this.a - this.a * factor;
-    if (this.a < 0) {
-      this.a = 0;
-    }
-  }
-
-  toJSON() {
-    if (this.a < 1) {
-      if (this.a === 0) {
-        return 'transparent';
-      } else {
-        return `rgba(${this.r},${this.g},${this.b},${this.a})`;
-      }
-    } else {
-      return `rgb(${this.r},${this.g},${this.b})`;
-    }
-  }
-
-  toString() {
-    return this.toJSON();
-  }
-
-  toHex() {
-    return `#${this.r.toString(16)}${this.g.toString(16)}${this.b.toString(16)}`;
-  }
-
-  // Getting a random color for debugging is quite useful sometimes
-  static random() {
-    var col = [0, 0, 0];
-
-    col = col.map(function() {
-      return ~~(Math.random() * 255);
-    });
-
-    return new Color(col[0], col[1], col[2]);
-  }
-}
 
 for (var colorName in colorConstants) {
   var colorValue = colorConstants[colorName];
