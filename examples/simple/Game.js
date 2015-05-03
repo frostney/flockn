a831abc7dc729db705e13c933777518799a2cc3b
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-03 23:32:09 +0200

    ES6-ify example

--- examples/simple/Game.js
+++ examples/simple/Game.js
@@ -12,28 +12,27 @@ flockn(function () {
 
     // Add a new object onto the scene
     this.addGameObject(function () {
-      var _this = this;
       // Set its name
       this.name = 'myGameObject';
 
       // Load an image for the texture
       this.texture.image.filename = 'hero_front.png';
 
-      this.input.key.on('down', function (keyCode) {
+      this.input.key.on('down', keyCode => {
         if (keyCode === this.up || keyCode === this.w) {
-          _this.position.y -= 10;
+          this.position.y -= 10;
         }
 
         if (keyCode === this.down || keyCode === this.s) {
-          _this.position.y += 10;
+          this.position.y += 10;
         }
 
         if (keyCode === this.left || keyCode === this.a) {
-          _this.position.x -= 10;
+          this.position.x -= 10;
         }
 
         if (keyCode === this.right || keyCode === this.d) {
-          _this.position.x += 10;
+          this.position.x += 10;
         }
       });
 
@@ -41,8 +40,8 @@ flockn(function () {
         alert('You clicked that hero guy.');
       });
 
-      this.on('update', function (dt) {
-        return _this.angle += (dt * 10);
+      this.on('update', dt => {
+        this.angle += (dt * 10);
       });
     });
 
