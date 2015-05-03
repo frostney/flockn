89f5c7d039c366952806d4a9a5b2bf7b76ab07c1
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-03 22:41:43 +0200

    Set renderer shortcut

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import GameObject from './gameobject';
 import Scene from './scene';
 import Behavior from './behavior';
 
+import Renderer from './renderer';
+
 var flockn = function flockn(descriptor) {
   return new Game(descriptor);
 };
 
+// TODO: Comtemplate if this should be a property
+flockn.setRenderer = function(name) {
+  Renderer.use(name);
 };
 
 export default flockn;
