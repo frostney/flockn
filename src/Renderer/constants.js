a8d25d4269570cb476e55af55c29828569a97cda
Johannes Stein <johannesstein@freeze-dev.com>
2018-01-08 00:47:23 +0000

    Add constants

---
+++ src/Renderer/constants.js
@@ -0,0 +1,5 @@
+export const INITIALIZE = Symbol('INITIALIZE');
+export const RENDER = Symbol('RENDER');
+export const ELEMENT_INSERT = Symbol('ELEMENT_INSERT');
+export const ELEMENT_REMOVE = Symbol('ELEMENT_REMOVE');
+export const TEXTURE_LOAD = Symbol('TEXTURE_LOAD');
